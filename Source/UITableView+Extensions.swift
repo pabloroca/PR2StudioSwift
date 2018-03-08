1b0736d72ca643d5f83d0b45ae57c1a8bc9d5b13
Pablo Roca <pablorocar@gmail.com>
2018-03-08 19:03:08 +0100

    fixes indent

--- Source/UITableView+Extensions.swift
+++ Source/UITableView+Extensions.swift
@@ -24,78 +24,77 @@ extension UITableView {
         }
     }
 
-    extension UITableView {
-        // MARK: - register & dequeue Cells and Header / Footer Cells
+    // MARK: - register & dequeue Cells and Header / Footer Cells
 
-        /// Gets the identifier for a cell class
-        ///
-        /// - Parameter cellClass: Cell Class (AnyClass)
-        /// - Returns: identifier as String
-        private func reuseIdentifierForCellClass(cellClass: AnyClass) -> String {
-            return String(describing: cellClass.self)
+    /// Gets the identifier for a cell class
+    ///
+    /// - Parameter cellClass: Cell Class (AnyClass)
+    /// - Returns: identifier as String
+    private func reuseIdentifierForCellClass(cellClass: AnyClass) -> String {
+        return String(describing: cellClass.self)
+    }
+
+    // MARK: - register & dequeue Cells
+
+    /// Registers a cell class for dequeueing. The reuseidentifier will be the class name of the type
+    ///
+    /// - Parameter type: The cell class to register
+    func registerCellClass<T: UITableViewCell>(ofType type: T.Type) {
+        self.register(T.self, forCellReuseIdentifier: self.reuseIdentifierForCellClass(cellClass: type))
+    }
+
+    /// Generic method to dequeue UITableViewCell. Making sure that all force casts are isolated in this extension
+    ///
+    /// - Parameters:
+    ///   - identifier: A string identifying the cell object to be reused.
+    ///   - indexPath: The index path specifying the location of the cell.
+    /// - Returns: A UITableviewCell instance of an expected type
+    func dequeueReusableCell<T: UITableViewCell>(withIdentifier identifier: String, for indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? T else {
+            fatalError("Expected cell \(identifier) at index \(indexPath)")
         }
+        return cell
+    }
 
-        // MARK: - register & dequeue Cells
-
-        /// Registers a cell class for dequeueing. The reuseidentifier will be the class name of the type
-        ///
-        /// - Parameter type: The cell class to register
-        func registerCellClass<T: UITableViewCell>(ofType type: T.Type) {
-            self.register(T.self, forCellReuseIdentifier: self.reuseIdentifierForCellClass(cellClass: type))
+    /// Dequeues a cell of a given type. The reuseidentifier used for dequeueing will be the class name.
+    ///
+    /// - Parameters:
+    ///   - type: The type of which a new cell should be dequeued
+    ///   - indexPath: The indexpath specifying the location of the cell
+    /// - Returns: A UITableViewCell instance of the expected type
+    func dequeueReusableCell<T: UITableViewCell>(ofType type: T.Type, for indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withIdentifier: self.reuseIdentifierForCellClass(cellClass: type), for: indexPath) as? T else {
+            fatalError("Expected cell \(self.reuseIdentifierForCellClass(cellClass: type)) at index \(indexPath)")
         }
+        return cell
+    }
 
-        /// Generic method to dequeue UITableViewCell. Making sure that all force casts are isolated in this extension
-        ///
-        /// - Parameters:
-        ///   - identifier: A string identifying the cell object to be reused.
-        ///   - indexPath: The index path specifying the location of the cell.
-        /// - Returns: A UITableviewCell instance of an expected type
-        func dequeueReusableCell<T: UITableViewCell>(withIdentifier identifier: String, for indexPath: IndexPath) -> T {
-            guard let cell = self.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? T else {
-                fatalError("Expected cell \(identifier) at index \(indexPath)")
-            }
-            return cell
+    // MARK: - register & dequeue header/footer Cells
+
+    /// Registers a header/footer class for dequeueing. The reuseidentifier will be the class name of the type
+    ///
+    /// - Parameter type: The header/footer class to register
+    func registerHeaderFooterClass<T: UITableViewHeaderFooterView>(ofType type: T.Type) {
+        self.register(T.self, forHeaderFooterViewReuseIdentifier: self.reuseIdentifierForCellClass(cellClass: type))
+    }
+
+    /// Generic method to dequeue UITableViewHeaderFooterView. Making sure that all force casts are isolated in this extension
+    ///
+    /// - Parameter identifier: A string identifying the header object to be reused.
+    /// - Returns: A UITableViewHeaderFooterView instance of an expected type
+    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(withIdentifier identifier: String) -> T {
+        guard let cell = self.dequeueReusableHeaderFooterView(withIdentifier: identifier) as? T else {
+            fatalError("Expected cell \(identifier) at index \(indexPath)")
         }
+        return cell
+    }
 
-        /// Dequeues a cell of a given type. The reuseidentifier used for dequeueing will be the class name.
-        ///
-        /// - Parameters:
-        ///   - type: The type of which a new cell should be dequeued
-        ///   - indexPath: The indexpath specifying the location of the cell
-        /// - Returns: A UITableViewCell instance of the expected type
-        func dequeueReusableCell<T: UITableViewCell>(ofType type: T.Type, for indexPath: IndexPath) -> T {
-            guard let cell = self.dequeueReusableCell(withIdentifier: self.reuseIdentifierForCellClass(cellClass: type), for: indexPath) as? T else {
-                fatalError("Expected cell \(self.reuseIdentifierForCellClass(cellClass: type)) at index \(indexPath)")
-            }
-            return cell
-        }
-
-        // MARK: - register & dequeue header/footer Cells
-
-        /// Registers a header/footer class for dequeueing. The reuseidentifier will be the class name of the type
-        ///
-        /// - Parameter type: The header/footer class to register
-        func registerHeaderFooterClass<T: UITableViewHeaderFooterView>(ofType type: T.Type) {
-            self.register(T.self, forHeaderFooterViewReuseIdentifier: self.reuseIdentifierForCellClass(cellClass: type))
-        }
-
-        /// Generic method to dequeue UITableViewHeaderFooterView. Making sure that all force casts are isolated in this extension
-        ///
-        /// - Parameter identifier: A string identifying the header object to be reused.
-        /// - Returns: A UITableViewHeaderFooterView instance of an expected type
-        func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(withIdentifier identifier: String) -> T {
-            guard let cell = self.dequeueReusableHeaderFooterView(withIdentifier: identifier) as? T else {
-                fatalError("Expected cell \(identifier) at index \(indexPath)")
-            }
-            return cell
-        }
-
-        /// Dequeues a header/footer of a given type. The reuseidentifier used for dequeueing will be the class name.
-        ///
-        /// - Parameter type: The type of which a new header/footer should be dequeued
-        /// - Returns: A UITableViewHeaderFooterView instance of the expected type
-        func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(ofType type: T.Type) -> T {
-            return self.dequeueReusableHeaderFooterView(withIdentifier: self.reuseIdentifierForCellClass(cellClass: type))
-        }
+    /// Dequeues a header/footer of a given type. The reuseidentifier used for dequeueing will be the class name.
+    ///
+    /// - Parameter type: The type of which a new header/footer should be dequeued
+    /// - Returns: A UITableViewHeaderFooterView instance of the expected type
+    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(ofType type: T.Type) -> T {
+        return self.dequeueReusableHeaderFooterView(withIdentifier: self.reuseIdentifierForCellClass(cellClass: type))
+    }
 
 }
