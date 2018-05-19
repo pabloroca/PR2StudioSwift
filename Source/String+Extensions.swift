bedec07fd252e6a187415185c87f56e925b7f7cf
Pablo Roca <pablorocar@gmail.com>
2018-05-19 11:42:04 +0200

    Adding String and UIButton extensions

---
+++ Source/String+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  String+Extensions.swift
+//  PR2StudioSwift
+//
+//  Created by Pablo Roca Rozas on 19/5/18.
+//  Copyright © 2018 PR2Studio. All rights reserved.
+//
+
+import Foundation
+
+extension StringProtocol {
+    var ascii: [Int] {
+        return unicodeScalars.filter {$0.isASCII}.map {Int($0.value)}
+    }
+}
+extension Character {
+    var ascii: Int? {
+        return Int(String(self).unicodeScalars.filter {$0.isASCII}.first?.value ?? 0)
+    }
+}
---
+++ Source/UIButton+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIButton+Extensions.swift
+//  PR2StudioSwift
+//
+//  Created by Pablo Roca Rozas on 19/5/18.
+//  Copyright © 2018 PR2Studio. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func setBackgroundColor(color: UIColor, forState: UIControlState) {
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+        UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.setBackgroundImage(colorImage, for: forState)
+    }}
