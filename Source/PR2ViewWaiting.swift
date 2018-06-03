f7250735a5fd7e1d5b5d53a2e0b19b36070e7041
Pablo Roca <pablorocar@gmail.com>
2018-06-03 19:03:04 +0200

    View Waiting component + bum version 0.3.15

---
+++ Source/PR2ViewWaiting.swift
@@ -0,0 +1,67 @@
+//
+//  PR2ViewWaiting.swift
+//  PR2StudioSwift
+//
+//  Created by Pablo Roca Rozas on 3/6/18.
+//  Copyright © 2018 PR2Studio. All rights reserved.
+//
+
+import UIKit
+
+public class PR2ViewWaiting: UIView {
+
+    private lazy var lblWaiting: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.black
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.text = "Loading data ..."
+        return label
+    }()
+
+    private lazy var waitingIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.activityIndicatorViewStyle = .gray
+        return activityIndicator
+    }()
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureUI() {
+        backgroundColor = UIColor.white
+        isHidden = true
+
+        addSubview(waitingIndicator)
+        NSLayoutConstraint.activate([
+            waitingIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            waitingIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+
+        addSubview(lblWaiting)
+        NSLayoutConstraint.activate([
+            lblWaiting.topAnchor.constraint(equalTo: topAnchor),
+            lblWaiting.leadingAnchor.constraint(equalTo: leadingAnchor),
+            lblWaiting.trailingAnchor.constraint(equalTo: trailingAnchor),
+            lblWaiting.bottomAnchor.constraint(equalTo: waitingIndicator.topAnchor, constant: 15.0)
+            ])
+    }
+
+    // MARK: - Custom methods
+
+    public func show() {
+        isHidden = false
+        waitingIndicator.startAnimating()
+    }
+
+    public func hide() {
+        waitingIndicator.stopAnimating()
+        isHidden = true
+    }
+}
